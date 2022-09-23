7c3f8ffffdb93b234d94c712ab265e338939bddd
mickmccann <mickmccann@ymail.com>
2022-09-23 15:07:37 +0000

    Wrapped the addEventListeners inside a function called main.

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-rock_div.addEventListener('click', function() {
-    game("rock");
-});
-
-paper_div.addEventListener('click', function() {
-    game("paper");
-});
-
-scissors_div.addEventListener('click', function() {
-    game("scissors");
-});
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    });
+    
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    });
+    
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    });
+}
